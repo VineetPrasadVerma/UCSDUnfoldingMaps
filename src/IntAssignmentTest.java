bb898e283801e32ee25cf5f601a767db94849bcd
Vineet Prasad Verma <vineetpd1996@gmail.com>
2018-02-11 20:28:36 +0530

    Initial Commit

---
+++ src/IntAssignmentTest.java
@@ -0,0 +1,10 @@
+
+public class IntAssignmentTest {
+	public static void main(String[] args) {
+		int var1 = 17;
+		int var2 =  var1 + 1;
+		var1 = var2 + 1;
+		System.out.println("var1 is "+var1+" , var2 is "+var2);
+	}
+
+}
