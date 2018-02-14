c661d9692872389d849d6e02c22b569c599e323b
Vineet Prasad Verma <vineetpd1996@gmail.com>
2018-02-14 08:20:38 +0530

    Sun changing its color according to time

--- src/MyPApplet.java
+++ src/MyPApplet.java
@@ -1,16 +1,36 @@
 import processing.core.*;
 public class MyPApplet extends PApplet{
 	PImage img; 
+	
 	public void setup() {
+		
 		size(200,200);
 		background(200);
 		img = loadImage("palmTrees.jpg","jpg");
-	}
-	public void draw() {
 		img.resize(0, height);
 		image(img,0,0);
-		ellipse(width/4,height/5,width/5,height/5);
-		fill(255,209,0);
 	}
+	
+	public void draw() {
+		
+		int[] color = sunColorSec(second());
+		fill(color[0], color[1], color[2]);
+		ellipse(width/4,height/5,width/5,height/5);
+		
+	}
+	
+	public int[] sunColorSec(float seconds) {
+		
+		int rgb[] =  new int[3];
+		float differFrom30 = Math.abs(30 - seconds);
+		float ratio = differFrom30/30;
+		rgb[0] = (int)(ratio*255);
+		rgb[1] = (int)(ratio*255);
+		rgb[2] = 0;
+		return rgb;
+		
+	}
+	
+	
 	
 }
